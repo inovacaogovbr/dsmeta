e79beaefcd8d304f522505cb9b7e56db255b43f6
Roberto Macena Viana <roberto.viana@inovacaogov.com.br>
2022-07-22 12:01:26 -0300

    useState

--- frontend/src/components/NotificationButton/index.tsx
+++ frontend/src/components/NotificationButton/index.tsx
@@ -1,4 +1,4 @@
-import icon from '../../assets/img/notification-icon.svg'
+import icon from '../../assets/img/notification-icon.svg';
 
 import './styles.css'
 
@@ -12,4 +12,4 @@ function NotificationButton() {
     )
 }
 
-export default NotificationButton
+export default NotificationButton;
