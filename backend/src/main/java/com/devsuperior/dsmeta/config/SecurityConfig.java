ebfb74a3b9f08dabf5edc2ffef81b99ca71ab0ff
Roberto Macena Viana <roberto.viana@inovacaogov.com.br>
2022-07-27 00:37:44 -0300

    Security Config

--- backend/src/main/java/com/devsuperior/dsmeta/config/SecurityConfig.java
+++ backend/src/main/java/com/devsuperior/dsmeta/config/SecurityConfig.java
@@ -12,8 +12,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-//@Configuration
-//@EnableWebSecurity
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig {
 
 	@Bean
