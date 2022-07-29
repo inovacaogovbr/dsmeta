fded6b8e0e717818e19885ef76c41608cd016c7d
Roberto Macena Viana <roberto.viana@inovacaogov.com.br>
2022-07-29 16:23:31 -0300

    Paginacao

--- backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
+++ backend/src/main/java/com/devsuperior/dsmeta/repositories/SaleRepository.java
@@ -4,6 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.devsuperior.dsmeta.entities.Sale;
 
-public interface SaleRepository extends JpaRepository<Sale, Long> {
+public interface SaleRepository extends JpaRepository<Sale, Long>{
 
 }
